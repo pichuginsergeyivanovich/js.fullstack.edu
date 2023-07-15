d1f7152436520b534b662d30b5b0fbc138d7630f
SIPichugin <SIPichugin@greenatom.ru>
2023-07-15 21:22:02 +0300

    fix some

--- docker/ssh-server/data/entry-point.sh
+++ docker/ssh-server/data/entry-point.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-
-ssh-keygen -t rsa -f /root/.ssh/id_rsa -y >>/home/git/.ssh/authorized_keys && chown -R git /home/git/.ssh
+chmod 400 /root/.ssh/id_rsa
+touch /root/.ssh/id_rsa && ssh-keygen -t rsa -f /root/.ssh/id_rsa -y >>/home/git/.ssh/authorized_keys && chown -R git /home/git/.ssh
 
 cd /app/server && npm run dev &
 
